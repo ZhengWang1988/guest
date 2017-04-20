e7f500eb8241e0883663bc105cbfdd61e3e51f2b
ZhengWang1988 <zhengjameswang@163.com>
2017-04-20 16:46:23 +0800

    嘉宾管理页面完成

--- guest/urls.py
+++ guest/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     url(r'^login_action/$',views.login_action),
     url(r'^event_manage/$',views.event_manage),
     url(r'^search_name/$',views.search_name),
+    url(r'^guest_manage/$',views.guest_manage),
 	#添加路径
 ]

83660f49dfe17b07eff6fffda5da321fd7dc222e
wangzheng <mailofwangzheng@163.com>
2017-06-04 13:44:15 +0800

    ...

--- WalkRandom/second/python_repos.py
+++ WalkRandom/second/python_repos.py
@@ -13,10 +13,16 @@ print("Total repositories:",response_dict['total_count'])
 
 # 探索有关仓库的信息
 repo_dicts = response_dict['items']
-names,stars = [],[]
+print("Number of items:",len(repo_dicts))
+names,plot_dicts = [],[]
 for repo_dict in repo_dicts:
 	names.append(repo_dict['name'])
-	stars.append(repo_dict['stargazers_count'])
+	plot_dict = {
+		'value':repo_dict['stargazers_count'],
+		'label':repo_dict['description'],
+	}
+	plot_dicts.append(plot_dict)
+
 # print("Repositories returned:",len(repo_dicts))
 
 # 研究第一个仓库
@@ -60,5 +66,5 @@ chart = pygal.Bar(my_config,style=my_style)
 chart.title = 'Most-Starred Python Projects on Github'
 chart.x_labels = names
 
-chart.add('',stars)
+chart.add('',plot_dicts)
 chart.render_to_file('python_repos.svg')
